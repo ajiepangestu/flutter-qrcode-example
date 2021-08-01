5861f7f0f3580f143c98818b6fa16e2d7f4321cb
Ajie Pangestu <paangestu@gmail.com>
2021-08-01 16:47:41 +0700

    add libraries

---
+++ lib/qrcode_app.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_qrcode_example/qrcode/home_page.dart';
+
+class QrcodeApp extends MaterialApp {
+  QrcodeApp({Key? key}) : super(key: key, debugShowCheckedModeBanner: false, home: HomePage());
+}
