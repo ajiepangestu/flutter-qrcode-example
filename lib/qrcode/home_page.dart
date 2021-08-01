ce86849869d26403be32b6cd6ce694812a5326d7
Ajie Pangestu <paangestu@gmail.com>
2021-08-01 21:19:02 +0700

    add qrcode generator

--- lib/qrcode/home_page.dart
+++ lib/qrcode/home_page.dart
@@ -1,23 +1,69 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_qrcode_example/qrcode/home_view.dart';
+import 'package:flutter_qrcode_example/qrcode/qrcode_generator_page.dart';
+import 'package:flutter_qrcode_example/qrcode/qrcode_scanner_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late PageController _pageController;
+  int _selectedIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Qrcode Example'),
       ),
-      body: HomeView(),
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        children: [
+          QrcodeScannerPage(),
+          QrcodeGeneratorPage(),
+        ],
+      ),
       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        backgroundColor: Colors.white,
+        unselectedItemColor: Colors.black45,
+        selectedItemColor: Colors.blue,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+          _pageController.jumpToPage(index);
+        },
         items: [
           BottomNavigationBarItem(
-            icon: Icon(Icons.qr_code_scanner),
             label: 'Qrcode Scanner',
+            icon: Icon(
+              Icons.qr_code_scanner,
+            ),
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.qr_code),
             label: 'Qrcode Generator',
+            icon: Icon(
+              Icons.qr_code,
+            ),
           ),
         ],
       ),
--- lib/qrcode/qrcode_generator_page.dart
+++ lib/qrcode/qrcode_generator_page.dart
@@ -1,8 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
-class QrcodeGeneratorPage extends StatelessWidget {
+class QrcodeGeneratorPage extends StatefulWidget {
+  @override
+  _QrcodeGeneratorPageState createState() => _QrcodeGeneratorPageState();
+}
+
+class _QrcodeGeneratorPageState extends State<QrcodeGeneratorPage> {
+  late TextEditingController _textEditingController;
+  String _qrcodeData = "";
+
+  @override
+  void initState() {
+    super.initState();
+    _textEditingController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        Container(
+          padding: EdgeInsets.all(20),
+          child: TextField(
+            controller: _textEditingController,
+            obscureText: true,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Qrcode Text',
+            ),
+          ),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            setState(() {
+              _qrcodeData = _textEditingController.text;
+            });
+          },
+          child: Text('Generate'),
+        ),
+        QrImage(
+          data: _qrcodeData,
+          version: QrVersions.auto,
+          size: 250.0,
+        )
+      ],
+    );
   }
 }
